4d9a64a3019f72a0b03b4cc2d277f10db4f54750
wow-felipesouza <felipe.souza@wowsolution.com>
2023-12-03 16:45:56 -0300

    fix: Migration Inicial

--- src/Infra/FinancialControl.Infra.DbMigrator/Baseline.cs
+++ src/Infra/FinancialControl.Infra.DbMigrator/Baseline.cs
@@ -1,5 +1,6 @@
 ﻿using FluentMigrator;
 
+
 namespace FinancialControl.Infra.DbMigrator
 {
     [Migration(1)]
@@ -7,7 +8,79 @@ namespace FinancialControl.Infra.DbMigrator
     {
         public override void Up()
         {
-            throw new NotImplementedException();
+            Create.Table("Transaction_Type")
+                .WithColumn("transaction_type_id")
+                    .AsInt32()
+                    .PrimaryKey()
+                .WithColumn("description")
+                    .AsString(size: 255)
+                    .NotNullable();
+
+            Create.Table("Account")
+                .WithColumn("account_id")
+                    .AsGuid()
+                    .PrimaryKey()
+                .WithColumn("description")
+                    .AsString(size: 255)
+                    .NotNullable()
+                .WithColumn("created_at")
+                    .AsDateTime()
+                    .NotNullable()
+                .WithColumn("active")
+                    .AsBoolean()
+                    .NotNullable();
+
+            Create.Table("Transaction")
+                .WithColumn("transaction_id")
+                    .AsGuid()
+                    .PrimaryKey()
+                .WithColumn("account_id")
+                    .AsGuid()
+                    .NotNullable()
+                    .ForeignKey(primaryTableName: "Account", primaryColumnName: "account_id")
+                .WithColumn("transaction_type_id")
+                    .AsInt32()
+                    .NotNullable()
+                    .ForeignKey(primaryTableName: "Transaction_Type", primaryColumnName: "transaction_type_id")
+                .WithColumn("description")
+                    .AsString(size: 255)
+                    .NotNullable()
+                .WithColumn("value")
+                    .AsDecimal(18,2)
+                    .NotNullable()
+                .WithColumn("transaction_date")
+                    .AsDateTime()
+                    .NotNullable();
+
+            Create.Table("Account_Balance")
+                .WithColumn("account_balance_id")
+                    .AsInt32()
+                    .PrimaryKey()
+                    .Identity()
+                .WithColumn("account_id")
+                    .AsGuid()
+                    .NotNullable()
+                    .ForeignKey(primaryTableName: "Account", primaryColumnName: "account_id")
+                .WithColumn("balance")
+                    .AsDecimal(18, 2)
+                    .NotNullable()
+                .WithColumn("updated_at")
+                    .AsDateTime()
+                    .NotNullable();
+
+            Create.Index("IX_Transaction_account_id")
+                .OnTable("Transaction").InSchema("dbo")
+                .OnColumn("account_id").Ascending();
+
+            Create.Index("IX_Transaction_transaction_type_id")
+                .OnTable("Transaction").InSchema("dbo")
+                .OnColumn("transaction_type_id").Ascending();
+
+            Create.Index("IX_Account_Balance_account_id")
+                .OnTable("Account").InSchema("dbo")
+                .OnColumn("account_id").Ascending();
+
+
         }
 
     }
